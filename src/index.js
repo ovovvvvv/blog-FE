1bb55c5de5f567c29967219ee796d844572aff8b
ovovvvvv <ovovvvvv@naver.com>
2024-01-30 00:11:57 +0900

    리덕스 적용

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import rootReducer from './modules/index';
+
+const store = createStore (rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store = {store} >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
