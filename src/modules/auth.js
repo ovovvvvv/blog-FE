f4fa9937ba59806f2da2a7d3eb7c5ccd33b52dd4
ovovvvvv <ovovvvvv@naver.com>
2024-02-03 21:39:15 +0900

    리덕스로 회원가입, 로그인 폼 관리

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,14 +1,46 @@
 import { createAction, handleActions } from 'redux-actions';
+import { produce } from 'immer';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+// 액션 타입 정의
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+// 액션 생성 함수 정의
+export const changeField = createAction(
+    CHANGE_FIELD,
+    ({ form, key, value}) => ({
+        form,
+        key,
+        value,
+    })
+    );
 
-const initialState = {};
+export const initializeForm = createAction(INITIALIZE_FORM, form => form); // register / login    
 
+// 초기 상태 정의
+const initialState = {
+    register : {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+    },
+    login : {
+        username: '',
+        password: '',
+    },
+};
+
+// 리듀서 정의
 const auth = handleActions(
     {
-        [SAMPLE_ACTION]: (state, action) => state,
+        [CHANGE_FIELD]: (state, { payload : {form, key, value}}) =>
+        produce(state, draft => {
+            draft[form][key] = value; // 예: state.register.username을 바꾼다
+        }),
+        [INITIALIZE_FORM] : (state, { payload : form }) => ({
+            ...state,
+            [form] : initialState[form],
+        }),
     },
     initialState,
 );
