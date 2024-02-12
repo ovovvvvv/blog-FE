bc660fa8bdc22e4d85b764e0b28923cde8f64523
ovovvvvv <ovovvvvv@naver.com>
2024-02-12 15:27:22 +0900

    로그인 구현, 에러처리

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,45 +1,73 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector} from 'react-redux';
-import {changeField, initializeForm} from '../../modules/auth';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
-    const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form : auth.login
-    }));
-    // 인풋 변경 이벤트 핸들러
-    const onChange = e => {
-        const { value, name} = e.target;
-        dispatch(
-            changeField({
-                form: 'login',
-                key: name,
-                value
-            })
-        );
-    };
+  const [error, setError] = useState(null);
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+    form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
+  }));
+  // 인풋 변경 이벤트 핸들러
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: 'login',
+        key: name,
+        value,
+      }),
+    );
+  };
 
-// 폼 등록 이벤트 핸들러
-const onSubmit = e => {
+  // 폼 등록 이벤트 핸들러
+  const onSubmit = (e) => {
     e.preventDefault();
-    // 구현 예정
-};
+    const { username, password } = form;
+    dispatch(login({ username, password }));
+  };
 
-// 컴포넌트가 처음 렌더링 될 때 form을 초기화함
-useEffect(() => {
+  // 컴포넌트가 처음 렌더링 될 때 form을 초기화함
+  useEffect(() => {
     dispatch(initializeForm('login'));
-}, [dispatch]);
+  }, [dispatch]);
 
-return (
-    <AuthForm 
-        type="login"
-        form={form}
-        onChange = {onChange}
-        onSubmit = {onSubmit}
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      setError('로그인 실패');
+      return;
+    }
+    if (auth) {
+      console.log('로그인 성공');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [navigate, user]);
+
+  return (
+    <AuthForm
+      type="login"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      error={error}
     />
-);
+  );
 };
 
 export default LoginForm;
